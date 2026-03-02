dfb5265f12527c476dcdbf706d863f2203811eda
Fabiano Krzyzanovski <mazufabiano@gmail.com>
2026-03-02 16:06:39 -0300

    Implementação do método abstrato 'aplicar_desconto' e adição da classe 'Sobremesa'

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from modelos.restaurante import Restaurante
 from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.0, 'Grande')
+bebida_suco.aplicar_desconto()
 prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+sobremesa_pudim = Sobremesa('Pudim de Leite', 15.0, 'Pudim', 'Médio', 'O melhor pudim da cidade')
+sobremesa_pudim.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(sobremesa_pudim)
+
 
 def main():
     restaurante_praca.exibir_cardapio
